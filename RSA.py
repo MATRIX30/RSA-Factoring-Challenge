e5c82b30228cb026704ad84e494380c75dcf501d
Matrix30 <Matrixboy30@gmail.com>
2023-09-30 08:29:14 +0100

    finish

--- RSA.py
+++ RSA.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import sys
 import math
-if (len(sys.argv) != 2):
+
+if len(sys.argv) != 2:
     print("Usage: factors <file>")
     exit(1)
-file = open(sys.argv[1], 'r')
+file = open(sys.argv[1], "r")
 content = file.read()
-content = content.split('\n')
+content = content.split("\n")
 for number in content:
-    for factor in range(2,(int(math.sqrt(int(number))))+1):
-        if int(number)%factor == 0:
-            print("{:d} = {:d}*{:d}".format(int(number),int(number)//factor, factor))
-            break;
+    print(type(number))
+    int_num = int(number)
+    for factor in range(2, (int(math.sqrt(int_num))) + 1):
+        if int_num % factor == 0:
+            print("{:d} = {:d}*{:d}".format(int_num, int_num // factor, factor))
+            break
